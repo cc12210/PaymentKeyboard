45b34279637317394ed0d21809accc2c520af2b1
曹小诚 <42371985+cc12210@users.noreply.github.com>
2025-09-12 09:38:19 +0000

    feat: 初始化显示

---
+++ src/components/PaymentKeyboard/index.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const PaymentKeyboard = () => {
+  const [amountValue, setAmountValue] = useState("");
+  return <div>PaymentKeyboard</div>;
+};
+
+export default PaymentKeyboard;
