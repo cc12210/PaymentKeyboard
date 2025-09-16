ebad931978d14184e58f8b28754dd6530a2e4aed
caocheng <2832913976@qq.com>
2025-09-16 18:22:56 +0800

    feat: 新增测试手撕代码方法

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import { useEffect, useState } from "react";
 import TransferAccount from "./pages/TransferAccount";
+import './utils/test';
 import "./storageManage";
 
 const str = "hello world";
 const strList = str.split("");
-console.log(strList);
+// console.log(strList);
 
 function App() {
   const [spliceLen, setSpliceLen] = useState<number>(0);
   const [showWord, setShowWord] = useState<string>("");
 
-  useEffect(() => {
-    if (spliceLen >= str.length) return;
-    const timer = setTimeout(() => {
-      setShowWord((pre) => pre + strList[spliceLen]);
-      console.log(spliceLen)
-      setSpliceLen((pre) => pre + 1);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, [spliceLen]);
+  // useEffect(() => {
+  //   if (spliceLen >= str.length) return;
+  //   const timer = setTimeout(() => {
+  //     setShowWord((pre) => pre + strList[spliceLen]);
+  //     console.log(spliceLen)
+  //     setSpliceLen((pre) => pre + 1);
+  //   }, 1000);
+  //   return () => clearTimeout(timer);
+  // }, [spliceLen]);
   // return <TransferAccount />;
   return <>{showWord}</>;
 }
---
+++ src/utils/test.ts
@@ -0,0 +1,56 @@
+var url = "https: //zhidao.baidu.com/question/1768422895052400180.html?fr=iks&word=slice&ie=gbk"
+
+const getUrl = (url: string) => {
+  if (!url) return
+  const urlSplit = url.split('?')[1]
+  const paramsList = urlSplit.split('&')
+  const paramsObj: any = {}
+  paramsList.forEach(item => {
+    const itemSplit = item.split('=')
+    paramsObj[itemSplit[0]] = itemSplit[1]
+  })
+  return paramsObj
+}
+
+console.log(getUrl(url))
+
+
+
+
+const debounce = (fn: Function, delay: number) => {
+  let timer: number;
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+    }, delay);
+  }
+}
+
+const throttle = (fn: Function, seconds: number) => {
+  let startTime = 0;
+  return function () {
+    const nowTime = new Date().getTime();
+    const waitTime = seconds - (nowTime - startTime);
+    if (waitTime <= 0) {
+      fn()
+      startTime = nowTime
+    }
+  }
+}
+
+
+const flatArr = (arr: any) => {
+  let flatList: any = []
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flatList = flatList.concat(flatArr(arr[i]))
+    } else {
+      flatList.push(arr[i])
+    }
+  }
+  return flatArr
+}
+
